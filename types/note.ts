8bd5b24a7852d3fcf41a41d6675bf818cad1a646
Oleh_Kovtoniuk <kovtoniuk.oleh@gmail.com>
2025-09-11 00:27:31 +0200

    поправки від ментора, оновлення проекта 1

--- types/note.ts
+++ types/note.ts
@@ -1,16 +1,12 @@
-export type NoteTag = 'Work' | 'Personal' | 'Meeting' | 'Shopping' | 'Todo' | undefined;
+// Допустимі значення тегів
+export type NoteTag = 'Work' | 'Personal' | 'Meeting' | 'Shopping' | 'Todo';
 
+// Модель однієї нотатки
 export interface Note {
   id: string;
   title: string;
   content: string;
   tag: NoteTag;
   createdAt: string;
-  updatedAt?: string;
-}
-
-export interface FetchNotesResponse {
-  notes: Note[];
-  totalPages: number;
-  totalNotes?: number; // необов'язково, можна додати для статистики
+  updatedAt: string; // тепер обов’язкове
 }
